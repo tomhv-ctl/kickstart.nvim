10f31a98ba003305ceef743610274f517f182b42
Tom Haskins-Vaughan <thaskinsvaughan@crisistextline.org>
2024-08-29 13:56:36 -0400

    Enable intelliphense

--- init.lua
+++ init.lua
@@ -607,6 +607,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        intelephense = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
